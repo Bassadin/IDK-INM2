50c8c066758547c3a459433f9608e3fdc20ec4ed
Bastian Hodapp <bastianhodapp@gmail.com>
2022-11-12 14:12:16 +0100

    changing values working

--- Task_04/aufgabe2/input.cpp
+++ Task_04/aufgabe2/input.cpp
@@ -3,11 +3,23 @@
 #include <iostream>
 #include "var.h"
 
+#include <thread>
+
+GLfloat moveSpeed = 0.5;
+
 int input_main()
 {
     kmhDashboardValue = 50;
 
-    std::cout << "input_main() " << kmhDashboardValue << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+
+    kmhDashboardValue = 120;
+    rpmDashboardValue = 4000;
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+
+    kmhDashboardValue = 30;
+    rpmDashboardValue = 6000;
 
     return 0;
 }
