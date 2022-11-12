e0f901bf726aa58c2e34c0aaa6df5149e50516be
Bastian Hodapp <bastianhodapp@gmail.com>
2022-11-12 13:03:33 +0100

    include threading

--- Task_04/aufgabe2/main.cpp
+++ Task_04/aufgabe2/main.cpp
@@ -10,7 +10,38 @@
 // Header
 #include "./draw.h"
 
-int main(void)
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void* drawingThread(void* arg)
 {
-	return draw_main();
+    // detach the current thread
+    // from the calling thread
+    pthread_detach(pthread_self());
+  
+    draw_main();
+  
+    // exit the current thread
+    pthread_exit(NULL);
+}
+  
+void fun()
+{
+    pthread_t ptid;
+  
+    // Creating a new thread
+    pthread_create(&ptid, NULL, &drawingThread, NULL);
+  
+  
+    // Waiting for the created thread to terminate
+    // pthread_join(ptid, NULL);
+  
+    pthread_exit(NULL);
+}
+  
+int main()
+{
+    fun();
+    return 0;
 }
