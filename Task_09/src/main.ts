da66f7c94c50ac2cd3ebae00dc7f1295be4e4535
Bastian Hodapp <bastianhodapp@gmail.com>
2023-01-10 11:30:42 +0100

    use scss

--- Task_09/src/main.ts
+++ Task_09/src/main.ts
@@ -1,21 +1,21 @@
-import "./style.css";
+import "./style.scss";
 import typescriptLogo from "./typescript.svg";
 import { setupCounter } from "./counter";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
     <h1>Vite + TypeScript</h1>
     <div class="card">
-      <button id="counter" type="button"></button>
+    <button id="counter" type="button"></button>
     </div>
     <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
+    Click on the Vite and TypeScript logos to learn more
     </p>
   </div>
 `;
